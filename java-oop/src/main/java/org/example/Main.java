e8044e38e202061fe9f001f4165705203822359e
unname <lwer210@naver.com>
2024-08-06 08:52:00 +0900

    Update #3 - Main
    변수 클래스를 인터페이스로 변경
    #3은 마무리
    스프링으로 넘어감

--- java-oop/src/main/java/org/example/Main.java
+++ java-oop/src/main/java/org/example/Main.java
@@ -1,15 +1,14 @@
 package org.example;
 
 import org.example.logic.BubbleSort;
+import org.example.logic.JavaSort;
+import org.example.logic.Sort;
 
 import java.util.Arrays;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        BubbleSort<String> sort = new BubbleSort<>();
-
+        Sort<String> sort = new JavaSort<>();
 
         System.out.println("result : " + sort.sort(Arrays.asList(args)));
     }
